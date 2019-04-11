e1d436de4d900e965858d1a85b49cc30e123a9e8
Jason Voll <jason.voll1441@gmail.com>
2019-04-11 13:53:26 -0700

    holy crap! hit the slack API and decoded the response partially

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -76,7 +78,46 @@ func DeletePerson(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
+type test struct {
+	Number int `json:"number"`
+}
+
+type SlackApiResponse struct {
+	OK      bool   `json:"ok"`
+	Channel string `json:"channel"`
+	// Message *Message `json:"message"`
+	// Reactions []Reaction `json:"reactions"`
+}
+
+func FetchEmojis() {
+	// (originally) Stolen from https://blog.alexellis.io/golang-json-api-client/
+	// url := "http://api.open-notify.org/astros.json"
+	url := "https://slack.com/api/reactions.get?token=xoxp-2322548031-380097601427-605976644912-eaf3c87c6e9ce58cc3615f46c7daffc8&channel=CHGG9F6BU&timestamp=1555014521.000700&pretty=1"
+	client := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatalf("Failed to create request %v", err)
+	}
+
+	res, getErr := client.Do(req)
+	if getErr != nil {
+		log.Fatalf("Failed to fetch emojis %v", getErr)
+	}
+
+	var slackMsg SlackApiResponse
+	err = json.NewDecoder(res.Body).Decode(&slackMsg)
+	if err != nil {
+		log.Fatalf("Failed to decode %v", err)
+	}
+	fmt.Println("Got this channel id back")
+	fmt.Println(slackMsg.Channel)
+}
+
 func main() {
+	FetchEmojis()
+
 	people = append(people, Person{ID: "1", FirstName: "Jason", LastName: "Voll", Address: &Address{City: "Cambridge", State: "MA"}})
 	people = append(people, Person{ID: "2", FirstName: "Payal", LastName: "Batra", Address: &Address{City: "Cambridge", State: "MA"}})
 	people = append(people, Person{ID: "3", FirstName: "Dan", LastName: "Dexter"})
