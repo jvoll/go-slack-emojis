acce6de5b78a2322dbd7bf152efd815ec8d9fbdf
Jesse Atkinson <jesse@jsatk.us>
2019-04-15 10:20:45 -0700

    WIP: Attempting to print list of emoji in order of most-used to least-used

--- main.go
+++ main.go
@@ -3,46 +3,56 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"sort"
 	"strings"
 	"time"
 )
 
-var TOKEN = "TOKEN_HERE"
+var slackAPIUrl = "https://slack.com/api/"
 
-// Reaction hush
+// Reaction - Contains the name of the reaction, the users who reacted with it,
+//            and the number of times it was reacted with.
 type Reaction struct {
 	Name  string   `json:"name"`
 	Users []string `json:"users"`
 	Count int      `json:"count"`
 }
 
-// Message hush
+// Message - Contains a list of Reactions.
 type Message struct {
 	Reactions []Reaction `json:"reactions"`
 }
 
-// ReactionsGetResponse hush
-type ReactionsGetResponse struct {
-	OK      bool     `json:"ok"`
-	Channel string   `json:"channel"`
-	Message *Message `json:"message"`
-}
-
-// Channel hush
+// Channel - Contains the ID & Name of a channel.
 type Channel struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
-// ChannelListResponse hush
+// ChannelListResponse - Contains list of Channels.
 type ChannelListResponse struct {
 	Channels []Channel `json:"channels"`
 }
 
+// ChannelHistoryResponse - Contains an array of the last 100 messages from the channel.
+type ChannelHistoryResponse struct {
+	Messages []Message `json:"messages"`
+}
+
+func getToken() string {
+	token, err := ioutil.ReadFile("oauth-access-token.txt")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(token)
+}
+
 func fetchChannelList() []Channel {
-	slackAPIUrl := "https://slack.com/api/"
 	channelsList := "channels.list"
 
 	var sb strings.Builder
@@ -50,7 +60,8 @@ func fetchChannelList() []Channel {
 	sb.WriteString(channelsList)
 	req, err := http.NewRequest(http.MethodGet, sb.String(), nil)
 	q := req.URL.Query() // Get a copy of the query values.
-	q.Add("token", TOKEN)
+	token := getToken()
+	q.Add("token", token)
 	q.Add("limit", "10")          // Limit us to 10 channels to not break the internet
 	req.URL.RawQuery = q.Encode() // Encode and assign back to the original query.
 
@@ -75,11 +86,6 @@ func fetchChannelList() []Channel {
 	return slackMsg.Channels
 }
 
-// ChannelHistoryResponse hush
-type ChannelHistoryResponse struct {
-	Messages []Message `json:"messages"`
-}
-
 func fetchChannelHistory(channelID string) []Message {
 	slackAPIUrl := "https://slack.com/api/"
 	channelsHistory := "channels.history"
@@ -89,7 +95,8 @@ func fetchChannelHistory(channelID string) []Message {
 	sb.WriteString(channelsHistory)
 	req, err := http.NewRequest(http.MethodGet, sb.String(), nil)
 	q := req.URL.Query() // Get a copy of the query values.
-	q.Add("token", TOKEN)
+	token := getToken()
+	q.Add("token", token)
 	q.Add("channel", channelID)
 	req.URL.RawQuery = q.Encode() // Encode and assign back to the original query.
 
@@ -100,6 +107,7 @@ func fetchChannelHistory(channelID string) []Message {
 	client := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
+
 	res, getErr := client.Do(req)
 	if getErr != nil {
 		log.Fatalf("Failed to fetch channels %v", getErr)
@@ -110,57 +118,29 @@ func fetchChannelHistory(channelID string) []Message {
 	if err != nil {
 		log.Fatalf("Failed to decode %v", err)
 	}
+
 	return channelHistory.Messages
 }
 
-// func fetchEmojis() {
-// 	slackAPIUrl := "https://slack.com/api/"
-// 	reactionsGet := "reactions.get"
-
-// 	var sb strings.Builder
-// 	sb.WriteString(slackAPIUrl)
-// 	sb.WriteString(reactionsGet)
-// 	req, err := http.NewRequest(http.MethodGet, sb.String(), nil)
-// 	q := req.URL.Query() // Get a copy of the query values.
-// 	q.Add("token", TOKEN)
-// 	q.Add("channel", "CHGG9F6BU")
-// 	q.Add("timestamp", "1555014521.000700")
-// 	req.URL.RawQuery = q.Encode() // Encode and assign back to the original query.
-
-// 	if err != nil {
-// 		log.Fatalf("Failed to create request %v", err)
-// 	}
-
-// 	client := http.Client{
-// 		Timeout: time.Second * 2, // Maximum of 2 secs
-// 	}
-// 	res, getErr := client.Do(req)
-// 	if getErr != nil {
-// 		log.Fatalf("Failed to fetch emojis %v", getErr)
-// 	}
-
-// 	var slackMsg ReactionsGetResponse
-// 	err = json.NewDecoder(res.Body).Decode(&slackMsg)
-// 	if err != nil {
-// 		log.Fatalf("Failed to decode %v", err)
-// 	}
-// 	fmt.Println("Got these reactions back.")
-// 	fmt.Println(slackMsg.Message.Reactions)
-// }
-
 func main() {
-	// fetchEmojis()
-
 	reactions := make(map[string]int)
-
 	channels := fetchChannelList()
+	// var m map[int]string
+	// var keys []int
+	// for k := range m {
+	// 	keys = append(keys, k)
+	// }
+	// sort.Ints(keys)
+	// for _, k := range keys {
+	// 	fmt.Println("Key:", k, "Value:", m[k])
+	// }
+
 	for _, channel := range channels {
 		recentMessages := fetchChannelHistory(channel.ID)
 
 		for _, message := range recentMessages {
 			// Only look at messages with emoji reactions
 			if message.Reactions != nil {
-
 				for _, reaction := range message.Reactions {
 					_, hasKey := reactions[reaction.Name]
 					if hasKey {
@@ -173,5 +153,19 @@ func main() {
 		}
 	}
 
-	fmt.Println(reactions)
+	fmt.Println("🐠", reactions)
+	inverse := make(map[int]string)
+	// numKeys := make([]int, len(reactions))
+	for k, v := range reactions {
+		inverse[v] += k + " | "
+		// append(numKeys, v)
+	}
+	fmt.Println("🎈", inverse)
+	// sort.Ints(numKeys)
+	// keys := sort.Reverse(sort.IntSlice(numKeys))
+	// fmt.Println("💎", numKeys)
+	// fmt.Println("😱", keys)
+	// for _, j := range numKeys {
+	// 	fmt.Println(inverse[j])
+	// }
 }
